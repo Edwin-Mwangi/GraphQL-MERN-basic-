e5a8dd3aaffcfe8d853abbf0c1c9ad288736a573
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-07-12 12:27:13 +0300

    root query

--- schema/schema.js
+++ schema/schema.js
@@ -11,3 +11,17 @@ const Book = new GraphQLObjectType({
     })
 })
 
+//root query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        //book field for query to get single book {Book(id:"123"){...}}
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString }}//1 arg is the id
+            resolve( parent, args) {
+                //to get data from db
+            }
+        }
+    }
+});
