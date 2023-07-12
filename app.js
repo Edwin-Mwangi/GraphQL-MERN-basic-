01abfae09b76f51b776698471be8224931f65b43
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-07-12 12:16:13 +0300

    setting up graphql

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+//graphql setup
+const graphqlHTTP = require('express-graphql')
+
+//graphqlHTTP used in middleware
+app.use('/graphql', graphqlHTTP({
+    //we need to pass schema in graphqlHTTP
+
+}));
+
 app.listen('4000', () => {
     console.log('listening requests at port 4000')
 })
